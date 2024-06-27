463b9e19f732a753acfdff208ec4be54d4775b19
Guoqiang QI <guoqiang.qi1@gmail.com>
2024-06-27 14:23:51 +0800

    Increase the heartbeat interval of message consumer from 60 seconds to 600.

--- src/message_queue.py
+++ src/message_queue.py
@@ -51,7 +51,7 @@ def publish_message(config, queue_name, message_body):
 
 def consumer(config, queue_name, callback_func):
     credentials = pika.PlainCredentials(config['username'], config['password'])
-    parameters = pika.ConnectionParameters(config['host'], int(config['port']), '/', credentials)
+    parameters = pika.ConnectionParameters(config['host'], int(config['port']), '/', credentials, heartbeat=600)
 
     try:
         connection = pika.BlockingConnection(parameters)
