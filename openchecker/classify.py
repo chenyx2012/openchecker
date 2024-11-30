9ce693f85403d78702034581db93a23d18c98af8
Guoqiang QI <guoqiang.qi1@gmail.com>
2024-11-30 17:10:24 +0800

    Classify with LLM.

--- openchecker/classify.py
+++ openchecker/classify.py
@@ -1,6 +1,7 @@
 from llm import ChatCompletionHandler
 import yaml
 import json
+from repo import get_generator, get_elasticsearch_client
 
 def extract_second_level_categories(yaml_file_path):
     categories = {}
@@ -17,19 +18,7 @@ def extract_second_level_categories(yaml_file_path):
 if __name__ == "__main__":
     yaml_file_path = "/home/guoqiang/opencheck/test/collections.yml"
     categories = extract_second_level_categories(yaml_file_path)
-    
-    all_projects = []
-    projects_file_path = "/home/guoqiang/opencheck/test/projects.json"
     result_projects_file_path = "/home/guoqiang/opencheck/test/result_projects.json"
-
-    with open(projects_file_path, "r") as f:
-        projects = json.load(f)
-        all_projects = [{
-                "name": project["_source"]["name"], 
-                "description": project["_source"]["description"],
-                "topics": project["_source"]["topics"],
-                "language": project["_source"]["language"]
-                } for project in projects ]
 
     few_shot_examples = """例如有如下项目信息：
         name：DataVisTool
@@ -52,7 +41,7 @@ if __name__ == "__main__":
 
     system_message = {
         "role": "system",
-        "content": f"""你是一个专业的项目分类助手，能够根据输入的开源项目的各项信息，准确地将其划分到合适的二级分类类别中,一个项目可以被划分到多个分类中。请严格按照以下格式输出结果，以方便后续的解析处理：
+        "content": f"""你是一个专业的项目分类助手，能够根据输入的开源项目的各项信息和给定的类别列表，准确地将项目划分到合适的二级分类类别中。一个项目可以被划分到多个分类中，且只能划分到给定的类别中，你要尽可能保证分类准确性。请严格按照以下格式输出结果，以方便后续的解析处理：
             {project_schema}
 
             给定的一级分类及二级分类信息及对应关系如下：
@@ -62,9 +51,50 @@ if __name__ == "__main__":
             """
     }
 
-
+    opensearch_url = ""
+    repo_index = "github_event_repository"
+    client = get_elasticsearch_client(opensearch_url)
+    body = {
+        "_source": ["name", "html_url", "description", "topics", "language"],
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "exists": {
+                            "field": "topics"
+                        }
+                    },
+                    {
+                        "exists": {
+                            "field": "language"
+                        }
+                    },
+                    {
+                        "exists": {
+                            "field": "description"
+                        }
+                    }
+                ]
+            }
+        },
+        "size": 100
+    }
+    
+    repo_generator = get_generator(client, body, repo_index)
     result_projects = []
-    for project in all_projects[:5]:
+    
+    count = 0
+    all_count = 0
+    for repo_item in repo_generator:
+        count += 1
+        all_count += 1
+        project = {
+                "name": repo_item["_source"]["name"], 
+                "description": repo_item["_source"]["description"],
+                "topics": repo_item["_source"]["topics"],
+                "language": repo_item["_source"]["language"]
+                }
+
         user_message = {
             "role": "user", 
             "content": """以下是需要进行分类的开源项目的详细信息：
@@ -89,5 +119,11 @@ if __name__ == "__main__":
         
         result_projects.append(project)
 
-    with open(result_projects_file_path, 'w', encoding='utf-8') as f:
-        json.dump(result_projects, f, ensure_ascii=True)
+        if count == 20:
+            with open(result_projects_file_path, 'w', encoding='utf-8') as f:
+                json.dump(result_projects, f, ensure_ascii=True)
+            
+            count = 0
+
+        if all_count == 5000:
+            exit()
---
+++ openchecker/repo.py
@@ -0,0 +1,144 @@
+import logging
+import urllib3
+import time
+from urllib.parse import urlparse
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+import logging
+import time
+import urllib3
+
+logger = logging.getLogger(__name__)
+urllib3.disable_warnings()
+
+
+def get_elasticsearch_client(elastic_url):
+    """ Get elasticsearch client by url """
+    is_https = urlparse(elastic_url).scheme == 'https'
+    client = Elasticsearch(
+        elastic_url, 
+        use_ssl=is_https, 
+        verify_certs=False, 
+        connection_class=RequestsHttpConnection,
+        timeout=100, 
+        max_retries=10, 
+        retry_on_timeout=True
+    )
+    return client
+
+def get_client(url):
+    """ Get default client by url """
+    global client
+    if client:
+        return client
+    client = get_elasticsearch_client(url)
+    return client
+
+def too_many_scrolls(res):
+    """Check if result conatins 'too many scroll contexts' error"""
+    r = res
+    return (
+        r
+        and 'status' in r
+        and 'error' in r
+        and 'root_cause' in r['error']
+        and len(r['error']['root_cause']) > 0
+        and 'reason' in r['error']['root_cause'][0]
+        and 'Trying to create too many scroll contexts' in r['error']['root_cause'][0]['reason']
+    )
+
+def free_scroll(client, scroll_id=None):
+        """ Free scroll after use"""
+        if not scroll_id:
+            return
+        try:
+            client.clear_scroll(scroll_id=scroll_id)
+        except Exception as e:
+            logger.debug("Error releasing scroll: {}", scroll_id)
+
+
+def get_items(client, index, body, size, scroll_id=None, scroll="5m"):
+        page = None
+        try:
+            if scroll_id is None:
+                page = client.search(index=index, body=body, scroll=scroll, size=size)
+            else:
+                page = client.scroll(scroll_id=scroll_id, scroll=scroll)
+        except Exception as e:
+            if too_many_scrolls(e.info):
+                return {'too_many_scrolls': True}
+        return page
+
+
+def get_generator(client, body, index=None):
+        scroll_wait = 900  #wait for 15 minutes
+        page_size = body["size"]
+        scroll_id = None
+        page = get_items(client = client, index=index, body=body, size=page_size, scroll_id=scroll_id)
+        if page and 'too_many_scrolls' in page:
+            sec = scroll_wait
+            while sec > 0:
+                logger.debug("Too many scrolls open, waiting up to {} seconds".format(sec))
+                time.sleep(1)
+                sec -= 1
+                page = get_items(index=index, body=body, size=page_size, scroll_id=scroll_id)
+                if not page:
+                    logger.debug("Waiting for scroll terminated")
+                    break
+                if 'too_many_scrolls' not in page:
+                    logger.debug("Scroll acquired after {} seconds".format(scroll_wait - sec))
+                    break
+
+        if not page:
+            return []
+        
+        scroll_id = page["_scroll_id"]
+        total = page['hits']['total']
+        scroll_size = total['value'] if isinstance(total, dict) else total
+
+        if scroll_size == 0:
+            free_scroll(client, scroll_id)
+            return []
+
+        while scroll_size > 0:
+
+            for item in page['hits']['hits']:
+                yield item
+            page = get_items(client=client, index=index, body=body, size=page_size, scroll_id=scroll_id)
+            if not page:
+                break
+
+            scroll_size = len(page['hits']['hits'])
+        free_scroll(client, scroll_id)
+
+if __name__ == '__main__':
+    opensearch_url = ""
+    repo_index = "github_event_repository"
+    
+    body = {
+        "_source": ["name", "html_url", "description", "topics", "language"],
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "exists": {
+                            "field": "topics"
+                        }
+                    },
+                    {
+                        "exists": {
+                            "field": "language"
+                        }
+                    },
+                    {
+                        "exists": {
+                            "field": "description"
+                        }
+                    }
+                ]
+            }
+        },
+        "size": 100
+    }
+    repo_generator = get_generator(get_elasticsearch_client(opensearch_url), body, repo_index)
+    for repo_item in repo_generator:
+        print(repo_item)
